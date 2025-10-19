4c9419656c2b06ea64931d757113659744ddb9a4
PrinceTiwari1235 <aaaaaq865@gmail.com>
2025-10-19 12:08:28 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// const a = 5;
+// const b = 6;
+// const c = a + b;
+// console.log(c)
+// const student = {
+//     name: "Prince",
+//     rollno: 1,
+//     address: "Chandrapur",
+//     level: "Bachlore"
+// }
+// console.log(student.name);
+// console.log(student.rollno);
+// console.log(student.address);
+// console.log(student.level);
+// const person = ["Manish", "Rahul", "Prince"]
+// console.log(person)
+// a = 5;
+// b = 5;
+// i = a + b;
+// console.log(i)
